22ebb7728d7939d8b014d8103c99d27c387bd47f
Don Coleman <dcoleman@chariotsolutions.com>
2014-10-14 16:16:42 -0400

    Send iOS advertising info as a dictionary

--- plugin/src/ios/CBPeripheral+Extensions.m
+++ plugin/src/ios/CBPeripheral+Extensions.m
@@ -7,9 +7,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,63 +21,123 @@
 static char ADVERTISING_IDENTIFER;
 static char ADVERTISEMENT_RSSI_IDENTIFER;
 
-@implementation CBPeripheral(com_megster_rfduino_extension)
+@implementation CBPeripheral(com_megster_ble_extension)
 
 -(NSDictionary *)asDictionary {
-    
+
     NSString *uuidString = NULL;
     if ([self UUID]) {
         uuidString = (__bridge_transfer NSString *)CFUUIDCreateString(NULL, self.UUID);
     } else {
         uuidString = @"";
     }
-    
+
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
     [dictionary setObject: uuidString forKey: @"id"];
-    
-    // TODO what should when this is null?
+
     if ([self name]) {
         [dictionary setObject: [self name] forKey: @"name"];
     }
-    
+
     if ([self RSSI]) {
         [dictionary setObject: [self RSSI] forKey: @"rssi"];
     } else if ([self advertisementRSSI]) {
         [dictionary setObject: [self advertisementRSSI] forKey: @"rssi"];
     }
-    
-    // TODO remove this RFduino specific code
+
     if ([self advertising]) {
         [dictionary setObject: [self advertising] forKey: @"advertising"];
     }
-    
+
     return dictionary;
 
 }
 
 // AdvertisementData is from didDiscoverPeripheral. RFduino advertises a service name in the Mfg Data Field.
 -(void)setAdvertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)rssi{
-    
-    // TODO send raw advertisement data, write a parser in JavaScript
 
-//    if (advertisementData) {
-//        id manufacturerData = [advertisementData objectForKey:CBAdvertisementDataManufacturerDataKey];
-//        if (manufacturerData) {
-//            const uint8_t *bytes = [manufacturerData bytes];
-//            int len = [manufacturerData length];
-//            // skip manufacturer uuid
-//            NSData *data = [NSData dataWithBytes:bytes+2 length:len-2];
-//            [self setAdvertising: [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
-//        }
-//    }
-    
-    // TODO Android sends back the raw bytes, iOS puts this in a dictionary
-    // See Advertisement Data Retrieval Keys https://developer.apple.com/library/mac/documentation/CoreBluetooth/Reference/CBCentralManagerDelegate_Protocol/translated_content/CBCentralManagerDelegate.html#//apple_ref/doc/constant_group/Advertisement_Data_Retrieval_Keys
-    [self setAdvertising:@"[]"];
+    [self setAdvertising:[self serializableAdvertisementData: advertisementData]];
     [self setAdvertisementRSSI: rssi];
+
 }
 
--(void)setAdvertising:(NSString *)newAdvertisingValue{
+// Translates the Advertisement Data from didDiscoverPeripheral into a structure that can be serialized as JSON
+//
+// This version keeps the iOS constants for keys, future versions could create more friendly keys
+//
+// Advertisement Data from a Peripheral could look something like
+//
+// advertising = {
+//     kCBAdvDataChannel = 39;
+//     kCBAdvDataIsConnectable = 1;
+//     kCBAdvDataLocalName = foo;
+//     kCBAdvDataManufacturerData = {
+//         CDVType = ArrayBuffer;
+//         data = "AABoZWxsbw==";
+//     };
+//     kCBAdvDataServiceData = {
+//         FED8 = {
+//             CDVType = ArrayBuffer;
+//             data = "ACAAYWJjBw==";
+//         };
+//     };
+//     kCBAdvDataServiceUUIDs = (
+//         FED8
+//     );
+//     kCBAdvDataTxPowerLevel = 32;
+//};
+- (NSDictionary *) serializableAdvertisementData: (NSDictionary *) advertisementData {
+
+    NSMutableDictionary *dict = [advertisementData mutableCopy];
+
+    // Service Data is a dictionary of CBUUID and NSData
+    // Convert to String keys with Array Buffer values
+    NSMutableDictionary *serviceData = [dict objectForKey:CBAdvertisementDataServiceDataKey];
+    if (serviceData) {
+        NSLog(@"%@", serviceData);
+
+        for(CBUUID *key in serviceData) {
+            [serviceData setObject:dataToArrayBuffer([serviceData objectForKey:key]) forKey:[key UUIDString]];
+            [serviceData removeObjectForKey:key];
+        }
+    }
+
+    // Create a new list of Service UUIDs as Strings instead of CBUUIDs
+    NSMutableArray *serviceUUIDs = [dict objectForKey:CBAdvertisementDataServiceUUIDsKey];
+    NSMutableArray *serviceUUIDStrings;
+    if (serviceUUIDs) {
+        serviceUUIDStrings = [[NSMutableArray alloc] initWithCapacity:serviceUUIDs.count];
+
+        for (CBUUID *uuid in serviceUUIDs) {
+            [serviceUUIDStrings addObject:[uuid UUIDString]];
+        }
+
+        // replace the UUID list with list of strings
+        [dict removeObjectForKey:CBAdvertisementDataServiceUUIDsKey];
+        [dict setObject:serviceUUIDStrings forKey:CBAdvertisementDataServiceUUIDsKey];
+
+    }
+
+    // Convert the manufacturer data
+    NSData *mfgData = [dict objectForKey:CBAdvertisementDataManufacturerDataKey];
+    if (mfgData) {
+        [dict setObject:dataToArrayBuffer([dict objectForKey:CBAdvertisementDataManufacturerDataKey]) forKey:CBAdvertisementDataManufacturerDataKey];
+    }
+
+    return dict;
+}
+
+// Borrowed from Cordova messageFromArrayBuffer since Cordova doesn't handle NSData in NSDictionary
+id dataToArrayBuffer(NSData* data)
+{
+    return @{
+             @"CDVType" : @"ArrayBuffer",
+             @"data" :[data base64EncodedString]
+             };
+}
+
+
+-(void)setAdvertising:(NSDictionary *)newAdvertisingValue{
     objc_setAssociatedObject(self, &ADVERTISING_IDENTIFER, newAdvertisingValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
