9735c72781009b935467e9066e47c5b0ed346348
Philip Peitsch <philip.peitsch@gmail.com>
2022-07-05 10:24:10 +1000

    Refactor manifest modification hook and support stripping out Android-12 attributes

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -1,107 +1,46 @@
 #!/usr/bin/env node
 'use strict';
 
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
 
-var ACCESS_BACKGROUND_LOCATION_PERMISSION =
-    '<uses-permission android:name="android.permission.ACCESS_BACKGROUND_LOCATION" />';
+const ACCESS_BACKGROUND_LOCATION = /\n.*?android\.permission\.ACCESS_BACKGROUND_LOCATION.*?\n/;
+const USES_PERMISSION_FLAGS = /\s*android:usesPermissionFlags="neverForLocation"\s*/;
 
 module.exports = function (context) {
-    const accessBackgroundLocationVariable = getAccessBackgroundLocationVariable();
-    const manifestPath = getAndroidManifestFilePath(context);
-    const androidManifest = fs.readFileSync(manifestPath).toString();
-    if (accessBackgroundLocationVariable === 'true') {
-        if (!accessBackgroundLocationExists(androidManifest)) {
-            addAccessBackgroundLocationToManifest(manifestPath, androidManifest);
-            console.log(context.opts.plugin.id + ': ACCESS_BACKGROUND_LOCATION permission added to ' + manifestPath);
-        } else {
-            console.log(
-                context.opts.plugin.id + ': ACCESS_BACKGROUND_LOCATION permission already exists in ' + manifestPath
-            );
-        }
-    } else {
-        if (accessBackgroundLocationExists(androidManifest)) {
-            removeAccessBackgroundLocationToManifest(manifestPath, androidManifest);
-            console.log(
-                context.opts.plugin.id + ': ACCESS_BACKGROUND_LOCATION permission removed from ' + manifestPath
-            );
-        } else {
-            console.log(
-                context.opts.plugin.id + ': ACCESS_BACKGROUND_LOCATION permission does not exists in ' + manifestPath
-            );
-        }
-    }
-};
+    const { projectRoot, plugin } = context.opts;
+    const { ConfigParser } = context.requireCordovaModule('cordova-common');
 
-var getAccessBackgroundLocationVariable = function () {
-    if (process.argv.join('|').indexOf('ACCESS_BACKGROUND_LOCATION=') > -1) {
-        return process.argv.join('|').match(/ACCESS_BACKGROUND_LOCATION=(.*?)(\||$)/)[1];
-    } else {
-        return getPreferenceValue('ACCESS_BACKGROUND_LOCATION');
+    const platformPath = path.resolve(projectRoot, 'platforms/android');
+    const gradleConfig = JSON.parse(fs.readFileSync(path.resolve(platformPath, 'cdv-gradle-config.json')).toString());
+    if (gradleConfig.SDK_VERSION == undefined) {
+        console.log(plugin.id + ': WARNING - unable to find Android SDK version');
     }
-};
+    const config = new ConfigParser(path.resolve(platformPath, 'app/src/main/res/xml/config.xml'));
 
-var getPreferenceValue = function (name) {
-    const config = fs.readFileSync('config.xml').toString();
-    var preferenceValue = getPreferenceValueFromConfig(config, name);
-    if (!preferenceValue) {
-        const packageJson = fs.readFileSync('package.json').toString();
-        preferenceValue = getPreferenceValueFromPackageJson(packageJson, name);
-    }
-    return preferenceValue;
-};
+    const accessBackgroundLocation = config.getPreference('accessBackgroundLocation', 'android');
 
-var getPreferenceValueFromConfig = function (config, name) {
-    const value = config.match(new RegExp('name="' + name + '" value="(.*?)"', 'i'));
-    if (value && value[1]) {
-        return value[1];
-    } else {
-        return null;
-    }
-};
-
-var getPreferenceValueFromPackageJson = function (packageJson, name) {
-    const value = packageJson.match(new RegExp('"' + name + '":\\s"(.*?)"', 'i'));
-    if (value && value[1]) {
-        return value[1];
-    } else {
-        return null;
-    }
-};
-
-var getAndroidManifestFilePath = function (context) {
-    const manifestPath = {
-        cordovaAndroid6: context.opts.projectRoot + '/platforms/android/AndroidManifest.xml',
-        cordovaAndroid7: context.opts.projectRoot + '/platforms/android/app/src/main/AndroidManifest.xml',
-    };
-    if (fs.existsSync(manifestPath.cordovaAndroid7)) {
-        return manifestPath.cordovaAndroid7;
-    } else if (fs.existsSync(manifestPath.cordovaAndroid6)) {
-        return manifestPath.cordovaAndroid6;
-    } else {
+    const manifestPath = path.resolve(platformPath, 'app/src/main/AndroidManifest.xml');
+    if (!fs.existsSync(manifestPath)) {
         throw "Can't find AndroidManifest.xml in platforms/Android";
     }
-};
 
-var accessBackgroundLocationExists = function (manifest) {
-    const value = manifest.search(ACCESS_BACKGROUND_LOCATION_PERMISSION);
-    if (value === -1) {
-        return false;
-    } else {
-        return true;
+    let manifestChanged = false;
+    let androidManifest = fs.readFileSync(manifestPath).toString();
+    if (accessBackgroundLocation != 'true' && androidManifest.search(ACCESS_BACKGROUND_LOCATION) != -1) {
+        console.log(plugin.id + ': ACCESS_BACKGROUND_LOCATION permission removed from ' + manifestPath);
+        androidManifest = androidManifest.replace(ACCESS_BACKGROUND_LOCATION, '\n');
+        manifestChanged = true;
     }
-};
 
-var addAccessBackgroundLocationToManifest = function (manifestPath, androidManifest) {
-    const index = androidManifest.search('</manifest>');
-    const accessBackgroundLocationPermissionLine = '    ' + ACCESS_BACKGROUND_LOCATION_PERMISSION + '\n';
-    const updatedManifest =
-        androidManifest.substring(0, index) + accessBackgroundLocationPermissionLine + androidManifest.substring(index);
-    fs.writeFileSync(manifestPath, updatedManifest);
-};
+    if (gradleConfig.SDK_VERSION < 31 && androidManifest.search(USES_PERMISSION_FLAGS) != -1) {
+        // (API 31) Build.VERSION_CODE.S Android 12
+        console.log(plugin.id + ': Stripping Android 12-only attributes from ' + manifestPath);
+        androidManifest = androidManifest.replace(USES_PERMISSION_FLAGS, ' ');
+        manifestChanged = true;
+    }
 
-var removeAccessBackgroundLocationToManifest = function (manifestPath, androidManifest) {
-    const accessBackgroundLocationPermissionLine = '    ' + ACCESS_BACKGROUND_LOCATION_PERMISSION + '\n';
-    const updatedManifest = androidManifest.replace(accessBackgroundLocationPermissionLine, '');
-    fs.writeFileSync(manifestPath, updatedManifest);
+    if (manifestChanged) {
+        fs.writeFileSync(manifestPath, androidManifest);
+    }
 };
